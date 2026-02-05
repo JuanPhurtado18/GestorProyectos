4fb2874065afab40b5e286850fd487123ebd3e0b
juanPhurtado18 <juanpablohurtado45@gmail.com>
2026-02-04 19:26:51 -0500

    feat: integrate Mapbox project markers with list navigation using Zustand

--- components/store/useProjectStore.jsx
+++ components/store/useProjectStore.jsx
@@ -2,7 +2,10 @@ import { create } from "zustand";
 
 export const useProjectStore = create((set) => ({
   search: "",
-  sortBy:"alphabetical",
+  sortBy: "alphabetical",
+  selectedProject: null, // 👈 proyecto seleccionado
+
   setSearch: (value) => set({ search: value }),
-  setSortBy: (value) => set ({sortBy:value}),
+  setSortBy: (value) => set({ sortBy: value }),
+  setSelectedProject: (project) => set({ selectedProject: project }), // 👈 setter
 }));
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,21 @@ import ProjectList from "../../components/projects/ProjectList";
 import data from "../../mock_data.json";
 import SearchBar from "../../components/projects/SearchBar";
 import SortSelect from "../../components/projects/ProjectFilters";
+import ProjectMap from "../../components/projects/ProjectMap";
 
 export default function Home() {
   const projects = data.projects ?? data;
 
   return (
-    <main>
-      <h1>Listado de Proyectos</h1>
-      <SortSelect/>
-      <SearchBar />
+    <main className="dashboard">
+      <h1 className="title">Mis proyectos</h1>
+
+      <div className="topbar">
+        <SortSelect />
+        <SearchBar />
+      </div>
+
+      <ProjectMap projects={projects} />
       <ProjectList projects={projects} />
     </main>
   );
