d73c644a62fc44b58e7fb68f7a36015cc4f16f47
juanPhurtado18 <juanpablohurtado45@gmail.com>
2026-02-06 15:31:15 -0500

    feat(ui): align filter and search layout with dashboard mockup

--- components/store/useProjectStore.jsx
+++ components/store/useProjectStore.jsx
@@ -3,9 +3,18 @@ import { create } from "zustand";
 export const useProjectStore = create((set) => ({
   search: "",
   sortBy: "alphabetical",
-  selectedProject: null, // 👈 proyecto seleccionado
+  selectedProject: null,
+
+  // 👇 NUEVO
+  mapVisible: false,
 
   setSearch: (value) => set({ search: value }),
   setSortBy: (value) => set({ sortBy: value }),
-  setSelectedProject: (project) => set({ selectedProject: project }), // 👈 setter
+  setSelectedProject: (project) => set({ selectedProject: project }),
+
+  // 👇 toggle mapa
+  toggleMap: () =>
+    set((state) => ({
+      mapVisible: !state.mapVisible,
+    })),
 }));
